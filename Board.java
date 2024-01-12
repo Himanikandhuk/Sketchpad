bdff8f1b593da74fcafe9e7fd2b312033b1b7805
Himanikandhuk <155885353+Himanikandhuk@users.noreply.github.com>
2024-01-12 16:32:23 +0530

    Board.java

--- Board.java
+++ Board.java
@@ -1,129 +1,150 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Board extends JFrame implements MouseListener, MouseMotionListener, ActionListener {
+public class Board extends JFrame implements ActionListener, AdjustmentListener {
     private Color currentColor;
+    int mousesize = 10;
 
     public Board() {
-        JButton blue = new JButton("Blue");
-        blue.setBackground(Color.BLUE);
-        blue.addActionListener(new ActionListener() {
+        JButton blue = createColorButton(Color.BLUE);
+        JButton white = createColorButton(Color.WHITE);
+        JButton green = createColorButton(Color.GREEN);
+        JButton red = createColorButton(Color.RED);
+        JButton orange = createColorButton(Color.ORANGE);
+        JButton pink = createColorButton(Color.PINK);
+        JButton eraser = createEraserButton();
+        JButton clear = createClearBUtton();
+        JScrollBar sizeScrollBar = createSizeScrollBar();
+
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        buttonsPanel.add(blue);
+        buttonsPanel.add(white);
+        buttonsPanel.add(green);
+        buttonsPanel.add(red);
+        buttonsPanel.add(orange);
+        buttonsPanel.add(pink);
+        buttonsPanel.add(eraser);
+        buttonsPanel.add(clear);
+        buttonsPanel.add(sizeScrollBar);
+
+        DrawingPanel drawingPanel = new DrawingPanel();
+        drawingPanel.setPreferredSize(new Dimension(900, 600));
+
+        getContentPane().setBackground(Color.BLACK);
+        setLayout(new BorderLayout());
+        add(buttonsPanel, BorderLayout.NORTH);
+        add(drawingPanel, BorderLayout.CENTER);
+
+        currentColor = Color.BLACK;
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private JScrollBar createSizeScrollBar() {
+        JScrollBar sizeScrollBar = new JScrollBar(JScrollBar.HORIZONTAL, mousesize, 1, 1, 51);
+        sizeScrollBar.setPreferredSize(new Dimension(300, 30));
+        sizeScrollBar.addAdjustmentListener(this);
+        return sizeScrollBar;
+    }
+
+    private JButton createClearBUtton() {
+        JButton clear = new JButton("Clear");
+        clear.setBackground(Color.GRAY);
+        clear.setPreferredSize(new Dimension(100, 50));
+        clear.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                currentColor = Color.BLUE;
+                currentColor = Color.BLACK;
+                repaint();
             }
         });
+        return clear;
 
-        JButton white = new JButton("White");
-        white.setBackground(Color.WHITE);
-        white.addActionListener(new ActionListener() {
+    }
+
+    private JButton createColorButton(Color color) {
+        JButton button = new JButton();
+        button.setBackground(color);
+        button.setPreferredSize(new Dimension(50, 50));
+        button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                currentColor = Color.WHITE;
+                currentColor = color;
             }
         });
+        return button;
+    }
 
-        JButton green = new JButton("Green");
-        green.setBackground(Color.GREEN);
-        green.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                currentColor = Color.GREEN;
-            }
-        });
-
-        JButton red = new JButton("Red");
-        red.setBackground(Color.RED);
-        red.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                currentColor = Color.RED;
-            }
-        });
-
-        JButton orange = new JButton("Orange");
-        orange.setBackground(Color.ORANGE);
-        orange.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                currentColor = Color.ORANGE;
-            }
-        });
-
-        JButton pink = new JButton("Pink");
-        pink.setBackground(Color.PINK);
-        pink.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                currentColor = Color.PINK;
-            }
-        });
-
+    private JButton createEraserButton() {
         JButton eraser = new JButton("Eraser");
-        eraser.setBackground(Color.WHITE);
+        eraser.setBackground(Color.GRAY);
+        eraser.setPreferredSize(new Dimension(100, 50));
         eraser.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 currentColor = Color.BLACK;
             }
         });
+        return eraser;
+    }
 
-        this.add(blue);
-        this.add(white);
-        this.add(green);
-        this.add(red);
-        this.add(orange);
-        this.add(pink);
-        this.add(eraser);
-        this.setPreferredSize(new Dimension(800, 600));
-        getContentPane().setBackground(Color.BLACK);
-        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
+    private class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
+        private int mouseX, mouseY;
 
-        currentColor = Color.BLACK;
+        public DrawingPanel() {
+            setBackground(Color.BLACK);
+            setPreferredSize(new Dimension(800, 600));
+            addMouseListener(this);
+            addMouseMotionListener(this);
+        }
 
-        addMouseListener(this);
-        addMouseMotionListener(this);
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(currentColor);
+            g.fillOval(mouseX - mousesize / 2, mouseY - mousesize / 2, mousesize, mousesize);
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            Graphics g = getGraphics();
+            g.setColor(currentColor);
+            g.fillOval(e.getX() - mousesize / 2, e.getY() - mousesize / 2, mousesize, mousesize);
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Unimplemented
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {
-        // Unimplemented
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // Unimplemented
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // Unimplemented
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // Unimplemented
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // Unimplemented
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        Graphics g = getGraphics();
-        g.setColor(currentColor);
-        g.fillOval(e.getX(), e.getY(), 20, 20);
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        // Unimplemented
+    public void adjustmentValueChanged(AdjustmentEvent e) {
+        mousesize = e.getValue();
     }
 }
+
